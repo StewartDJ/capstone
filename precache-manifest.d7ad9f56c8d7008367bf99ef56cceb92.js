665c28ff92b1f986d384e4137ff4ae27aead1d24
DJ Stewart <deshastewart1992@gmail.com>
2020-08-25 14:01:23 -0700

    Updates

---
+++ precache-manifest.d7ad9f56c8d7008367bf99ef56cceb92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e394b63b0fba539b7e67a7bc6be2ba",
+    "url": "/capstone/index.html"
+  },
+  {
+    "revision": "5a3c992da0422ccdb5e8",
+    "url": "/capstone/static/css/main.ce9e93c5.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/capstone/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/capstone/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3c992da0422ccdb5e8",
+    "url": "/capstone/static/js/main.58d7197f.chunk.js"
+  },
+  {
+    "revision": "8523c401c9bbda965c80",
+    "url": "/capstone/static/js/runtime-main.242b889f.js"
+  }
+]);
