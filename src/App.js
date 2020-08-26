ab3be0dc324c7d3b359a9b1ad23648d7ca224c55
DJ Stewart <deshastewart1992@gmail.com>
2020-08-26 10:53:47 -0700

    origin master

--- src/App.js
+++ src/App.js
@@ -19,9 +19,94 @@ function App() {
         </div>
       
       </header>
-    </div>
+    
+   
 
-    </Router>
+<div class="layoutTestYellow pageBody">
+   
+
+<div class="layoutTestBlue left pageBody">
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../assets/tourist2.jpg" alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide"/>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="layoutTestRed right pageBody">
+
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <p class="bottom-left">Words</p> 
+</div>
+</div>
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <img src="../react/assets/arcade.jpg" alt="arcade"/>
+    <p class="bottom-left">Words</p> 
+    
+</div>
+</div>
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <img src="../react/assets/skatepark3.jpg" alt="arcade"/>
+
+    <p class="bottom-left">Words</p> 
+</div>
+</div>
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <p class="bottom-left">Words</p> 
+</div>
+</div>
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <p class="bottom-left">Words</p> 
+</div>
+</div>
+
+<div class="imgWithText">
+   
+
+<div class="layoutElements">
+    <p class="bottom-left">Words</p> 
+</div>
+</div>
+
+</div>
+</div>
+<div class="layoutTestGreen footer">
+
+
+  
+</div>
+</div>
+</Router>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-
 export default class Navbar extends Component {
     render() {
         return (
             <nav class="navbar navbar-dark bg-dark justify-content-between">
+
                 <a class="navbar-brand" href="/">TravelSpot</a>
+                <div className="collapse navbar-collapse">
 
                 <ul className="navbar-nav mr-auto"> 
                         <li className="navbar-item">
@@ -29,7 +30,7 @@ export default class Navbar extends Component {
                             </li>
 
                         </ul>
-
+</div>
 
                 <form class="form-inline">
                   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
