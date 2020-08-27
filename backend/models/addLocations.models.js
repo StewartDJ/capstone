c686aef16d3c9e7005ec0f180daa3ab5854758f4
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-27 11:29:10 -0400

    Schema updates

--- backend/models/addLocations.models.js
+++ backend/models/addLocations.models.js
@@ -4,12 +4,21 @@ const Schema = mongoose.Schema;
 
 const addLocationsSchema = new Schema({
   Name: { type: String, required: true },
+  description: {type: String, required: true},
   website: { type: String, required: true },
-  phoneNumber: { type: Number, required: true },
-  date: { type: Date, required: true },
-}, {
-  timestamps: true,
-});
+  imageUrl:{type: String, required: true},
+  location: {object:
+  {
+    address: {type: String, required: true},
+    city: {type: String, required: true},
+    state:{type: String, required: true},
+    zipCode:{type:Number, required: true},
+  },},
+  indoors:{type: Boolean, required: true},    
+ familyFriendly:{type: Boolean, required: true}
+}) 
+ 
+
 
 
 const addLocations = mongoose.model('addLocations', addLocationsSchema);
