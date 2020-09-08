4d70b51cfbaf58f413d9c4dec239fb025a7215f0
Saeed Shareef <saeedhshareef@gmail.com>
2020-09-08 11:56:47 -0400

    tues morn push

--- src/components/locationsList.component.js
+++ src/components/locationsList.component.js
@@ -36,45 +36,25 @@ getLocationPost() {
     if (!locations.length) return null;
     return locations.map((destination, index) => {
         return(
-
-
       <div key={index} className="theLocations">
          <div className="yellowEdge"></div>
          <div className= "innerContents">
-    
 <div className="scroll">
   <div className="grid-item"><h2>{destination.name} </h2></div>
-  
    <div className="grid-item">     
-   {/* {this.state.location} */}
-   
-   {JSON.stringify(destination.location, function (key, value) {
-   if (key === "city" || key === "state" || key === "address") {
-   return value.toUpperCase()
-    
-
-   } else {
-     return value;
-   }}
-   )}
-     
 </div> 
+<div className="grid-item uppercase"><center> {destination.location.address} {destination.location.city} {destination.location.state} {destination.location.zipCode} </center></div>
 <br/>
   <div className="grid-item"> <p>{destination.description}</p> </div>
   <div className="grid-item">  <a href= {destination.website} target="_blank" className="btn btn-warning" rel="noopener noreferrer"> VISIT WEBSITE</a> </div>
- 
 </div>
-
       <div> 
          </div>
              <br />
-         
 </div>
 <div className="imageDiv">
    <img src=  {destination.imageUrl} alt="location"   /> </div>
       </div> 
-
-      
         )
     });
   };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,18 +3,14 @@ import { Link } from 'react-router-dom'
 import {UncontrolledDropdown,DropdownToggle,DropdownMenu, DropdownItem,} from 'reactstrap'
 import { Nav } from 'react-bootstrap'
 import axios from 'axios';
-
 export default class Navbar extends Component {
-
     state = { 
         name: ' ',
         locations: [] 
       }
-    
     componentDidMount(){
       this.getLocationPost();
     }
-    
     getLocationPost() {
         axios.get('http://localhost:5000/all-locations')
        .then((res) => {
@@ -27,9 +23,14 @@ export default class Navbar extends Component {
           alert('error ABANDON SHIP!!!')
         });
       }
-        
+    searchbar(locations) {
+      if (!locations.length) return null;
+       return locations.map((destination, index) => {
+        console.log(destination.name)
+       return <option value = {destination.name}/>
+      })
+    }
     render() {
-        
         return (
             <nav className="navbar navbar-light bg-warning justify-content-between navbar-expand-lg">
                 <Link to="/" className="navbar-brand " > <img src={require('../assets/planeicon.png')} width="40px" alt="icon" class="icon"/>  EXPLORE CINCINNATI  </Link>
@@ -73,23 +74,18 @@ export default class Navbar extends Component {
                                 </DropdownMenu>
                                 </UncontrolledDropdown>
                                             </ul>
-
             </div>
-            
                 <form className="form-inline" method="get" action="https://google.com/search" method="get" autocomplete="on">
                 <input list="places" className="form-control mr-sm-2" type="text" placeholder="Search Google" aria-label="Search Google" id="searchName" name="q"/> 
-                
 <datalist id="places">
 <option value="Hotels in Cincinnati"/>
 <option value="Public Transportation in Cincinnati"/>
 <option value="Cincinnati History"/>
 <option value="Cincinnati Facts"/>
 <option value="Airports in Cincinnati"/>
+{this.searchbar(this.state.locations)}
 </datalist>
-
                 <button className="btn btn-info buttons" type="submit" value="Google Search">Search</button>
-                
-                
                 </form>
                 </nav>
         )
